777350eac989ec299c239c9ed2c984fb206be715
ckangwen <2437037469@qq.com>
2023-10-20 11:37:23 +0800

    feat: add proxy worker

---
+++ README.md
@@ -0,0 +1,5 @@
+# cloudflare workers
+
+## proxy
+
+用于代理请求，解决国内请求某些网站或接口被墙的问题
--- proxy/src/index.ts
+++ proxy/src/index.ts
@@ -10,7 +10,6 @@
 
 import handleProxy from './proxy';
 import handleRedirect from './redirect';
-import apiRouter from './router';
 
 // Export a default object containing event handlers
 export default {
@@ -29,17 +28,12 @@ export default {
 				return handleProxy.fetch(request, env, ctx);
 		}
 
-		if (url.pathname.startsWith('/api/')) {
-			// You can also use more robust routing
-			return apiRouter.handle(request);
-		}
 
 		return new Response(
 			`Try making requests to:
       <ul>
       <li><code><a href="/redirect?redirectUrl=https://example.com/">/redirect?redirectUrl=https://example.com/</a></code>,</li>
-      <li><code><a href="/proxy?modify&proxyUrl=https://example.com/">/proxy?modify&proxyUrl=https://example.com/</a></code>, or</li>
-      <li><code><a href="/api/todos">/api/todos</a></code></li>`,
+      <li><code><a href="/proxy?modify&proxyUrl=https://example.com/">/proxy?modify&proxyUrl=https://example.com/</a></code>, or</li>`,
 			{ headers: { 'Content-Type': 'text/html' } }
 		);
 	},
